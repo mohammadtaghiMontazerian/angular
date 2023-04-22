2ab7f93c85f5311725cb16830d66d54868cd66e4
Mohammadtaghi Montazerian <m.ap.montazerian@gmail.com>
2023-04-22 12:05:14 +0100

    Connected to BackEnd Add and Delete to InMemoryDB

--- src/app/mediaitems/media-item-list.component.ts
+++ src/app/mediaitems/media-item-list.component.ts
@@ -1,23 +1,51 @@
-import { Component, EventEmitter, Output, OnInit, Inject } from "@angular/core";
+import { Component, OnInit, Inject, Injector } from "@angular/core";
 //import { mediaItemService } from "./media-item-service";
 import { lookupListToken } from "./providers";
 import { ActivatedRoute } from "@angular/router";
 import { MediaItemServiceProxy, MediaItemDtoPagedResultDto, MediaItemDto} from '@shared/service-proxies/service-proxies';
 import { finalize } from "rxjs";
+import { PagedListingComponentBase, PagedRequestDto } from "@shared/paged-listing-component-base";
+
+class PagedMediaItemRequestDto extends PagedRequestDto {
+    keyword: string;
+    isActive: boolean | null;
+  }
 
 @Component({
     selector: 'mw-media-item-list',
     templateUrl: './media-item-list.component.html',
     styleUrls: ['./media-item-list.component.css'],
 })
-export class MediaItemListComponent implements OnInit {
+export class MediaItemListComponent extends PagedListingComponentBase<MediaItemDto>
+implements OnInit {
+    
+    keyword = '';
+    isActive: boolean | null;
     mediaItems: MediaItemDto[]
     searchMedium: any = ''
 
-    constructor(private mediaItemService1: MediaItemServiceProxy,
+    constructor(injector: Injector,
+        private mediaItemService1: MediaItemServiceProxy,
         @Inject(lookupListToken) public lookupLists:any,
         //private mediaItemService: mediaItemService,
-        private activatedRoute: ActivatedRoute){}
+        private activatedRoute: ActivatedRoute){
+            super(injector);
+          }
+
+        l(key: string, ...args: any[]): string {
+            let localizedText = this.localization.localize(key, this.localizationSourceName);
+    
+            if (!localizedText) {
+                localizedText = key;
+            }
+    
+            if (!args || !args.length) {
+                return localizedText;
+            }
+    
+            args.unshift(localizedText);
+            return abp.utils.formatString.apply(this, args);
+        }
 
     ngOnInit(): void {
         //this.searchMedium ="Movies";
@@ -31,7 +59,7 @@ export class MediaItemListComponent implements OnInit {
                                     this.mediaItems = this.getMediaItems(medium)
                                 });
     }
-    @Output() delete = new EventEmitter();
+   // @Output() delete = new EventEmitter();
 
      getMediaItems(medium: any): MediaItemDto[]{
     //     console.log('getMediaItems called');
@@ -65,7 +93,11 @@ export class MediaItemListComponent implements OnInit {
         if (event.checked == event.target.value)
             event.checked = false
     }
-    onMediaItemListDelete(mediaItem: any) {
+    protected delete(mediaItem: MediaItemDto): void {
+        this.onMediaItemListDelete(mediaItem);
+    }
+
+    onMediaItemListDelete(mediaItem: MediaItemDto) {
         //this.delete.emit(mediaItem);
  /*       if (this.firstMediaItem.watchedOn == true)
             this.firstMediaItem.watchedOn = false;
@@ -73,12 +105,45 @@ export class MediaItemListComponent implements OnInit {
             this.firstMediaItem.watchedOn = true;
             */
         console.log(mediaItem.name)
-        this.mediaItemService1.delete(mediaItem)
+        //this.mediaItemService1.delete(mediaItem.id)
+        abp.message.confirm(
+            this.l('MediaItemDeleteWarningMessage', mediaItem.name),
+            undefined,
+            (result: boolean) => {
+              if (result) {
+                this.mediaItemService1.delete(mediaItem.id).subscribe(() => {
+                  abp.notify.success(this.l('SuccessfullyDeleted'));
+                  this.refresh();
+                });
+              }
+            }
+          );
         //.subscribe(()=>{this.getMediaItems(this.searchMedium)});
         console.log('this is Media Item List delete log');
       
     };
-   
+    protected list(
+        request: PagedMediaItemRequestDto,
+        pageNumber: number,
+        finishedCallback: Function
+      ): void {
+        request.keyword = this.keyword;
+        request.isActive = this.isActive;
+    
+        this.mediaItemService1
+        .getAll(
+            '',0,1000
+        )
+        .pipe(
+        finalize(() => {
+            finishedCallback();
+        })
+        )
+        .subscribe((result: MediaItemDtoPagedResultDto) => {
+        this.mediaItems = result.items;
+        //this.showPaging(result, pageNumber);
+        });
+      }   
 }
 
 function finishedCallback() {
